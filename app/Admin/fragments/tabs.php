3291403c9dcf28b5da10a49641539e043c8ccc74
susameca <susamecaood@gmail.com>
2025-02-24 13:18:20 +0200

    Box now: Add message in settings

--- app/Admin/fragments/tabs.php
+++ app/Admin/fragments/tabs.php
@@ -13,7 +13,12 @@ $current_tab_key = array_search( $current_tab, $slugs );
 $current_tab_object = $tabs[ $current_tab_key ];
 ?>
 <div class="wrap woocommerce">
-	<div class="notice notice-success"><?php echo wpautop( 'BOX NOW message' ) ?></div>
+	<div class="notice notice-success">
+		<h3>Интеграция с BOX NOW</h3>
+		<p>Възползвайте се от бърза и лесна интеграция с <strong>BOX NOW</strong>. <strong>BOX NOW</strong> е най-зелената (пряко и преносно) куриерска услуга в България.</p>
+		<p><strong>BOX NOW</strong> предлагат: Мрежа от 700+ автомата в 150 населени места. 24/7 достъп до автоматите. Фиксирана цена на доставка, без скрити такси. Доставка в същата вечер в рамките на София и до 24 часа за цялата страна. Съботни доставки без оскъпяване на куриерската услуга. Безплатно връщане на невзети пратки. Включено безплатно покритие до 800лв. на пратка. Безплатно известяване на клиентите (SMS, Viber, E-mail, BOX NOW app)</p>
+		<p> <a class="button button-primary" href="https://boxnow.bg/e-shops" target="_blank">Станете техен партньор сега</a> ( След сключване на договор активирайте интеграцията от таб "Настройки" )</p>
+</div>
 
 	<nav class="nav-tab-wrapper woo-nav-tab-wrapper">
 		<?php
