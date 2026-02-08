27b47669f2ced8231d8f771cb2e8658f079f337e
susameca <susamecaood@gmail.com>
2026-02-08 20:39:46 +0200

    Checkout: Fix label for shippings

--- app/Shipping/CheckoutLayout.php
+++ app/Shipping/CheckoutLayout.php
@@ -5,9 +5,15 @@ defined( 'ABSPATH' ) || exit;
 
 class CheckoutLayout {
 	public function __construct() {
+		add_filter('woocommerce_shipping_package_name', array( __CLASS__, 'change_shipping_package_name' ), 10, 2);
+		
 		if ( woo_bg_get_option( 'checkout', 'alternative_shipping_table' ) === 'yes' ) {
 			add_filter( 'woocommerce_locate_template', array( __CLASS__, 'intercept_shipping_table_template' ), 10, 3 );
 		}
+	}
+
+	public static function change_shipping_package_name( $name, $i ) {
+		return __('Shipping', 'woocommerce');
 	}
 
 	public static function intercept_shipping_table_template( $template, $template_name, $template_path ) {
